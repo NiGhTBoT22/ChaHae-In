4a7f9338d096de20afe3e4633d04075620c4a138
levelingdragon <77844089+levelingdragon@users.noreply.github.com>
2021-10-14 02:07:16 +0530

    Que Porra E essa?
    wtf ehh

--- AriseRobot/modules/__speachtotext.py
+++ AriseRobot/modules/__speachtotext.py
@@ -1,4 +1,4 @@
-from AsunaRobot import telethn as tbot
+from AriseRobot import telethn as tbot
 import os
 import urllib.request
 from datetime import datetime
@@ -11,8 +11,8 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from AsunaRobot import *
-from AsunaRobot.events import register
+from AriseRobot import *
+from AriseRobot.events import register
 
 
 async def is_register_admin(chat, user):
