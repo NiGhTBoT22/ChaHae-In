6f17ccdcf45823dfe19732e95379dc197ce2eeb6
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-10-21 01:24:00 +0530

    UwU baby Cha

--- AriseRobot/__main__.py
+++ AriseRobot/__main__.py
@@ -246,7 +246,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_video(
-            CHA_VID, caption=f"UwU Cʜᴀ Hᴀᴇ-Iɴ Summoned by {}(차해인)!\nProtecting Groups<b> since:</b> <code>{}</code>".format(
+            CHA_VID, caption= "UwU Cʜᴀ Hᴀᴇ-Iɴ Summoned by {}(차해인)!\nProtecting Groups<b> since:</b> <code>{}</code>".format(
                  escape_markdown(first_name), escape_markdown(uptime)
             ),
             parse_mode=ParseMode.MARKDOWN,
