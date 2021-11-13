dac40bdad018e181de6347f614c8230adbf8baa5
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-11-13 13:44:42 +0530

    Update misc.py

--- AriseRobot/modules/helper_funcs/misc.py
+++ AriseRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("⫸", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="help_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="AriseRobot_back")]]
 
     return pairs
 
